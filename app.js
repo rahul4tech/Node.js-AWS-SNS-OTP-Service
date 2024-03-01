514eb6f6d6e8b96bd4a7f2066bd16c391e9f2b78
Rahul Sinha <rahul@nextechinc.net>
2024-03-02 04:09:02 +0530

    added process.env.port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const verifyApiKey = require('./middleware/verifyApiKey');
 const app = express();
 require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 const AWS = require('aws-sdk');
 
@@ -44,4 +45,4 @@ app.post('/', verifyApiKey, async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('AWS SNS SMS OTP Service Listening on PORT 3000'));
+app.listen(port, () => console.log(`AWS SNS SMS OTP Service Listening on PORT ${port}`));
